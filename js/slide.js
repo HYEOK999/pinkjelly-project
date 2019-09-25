9c0fd05c3c9a26b28d8a8c8736b76b2d4ba2deaa
HYEOK999 <iasg2004@naver.com>
2019-09-25 16:27:28 +0900

    release : Add the CSS #12
    1. Add the CSS at adevertise
    2. Add the JS at Top-Bottom button

--- js/slide.js
+++ js/slide.js
@@ -31,5 +31,22 @@ window.addEventListener('scroll', function(){
     }
 })
 
+var button = document.querySelectorAll(".top, .bottom");
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 1){
+        console.log(4565);
+        button[0].classList.add('button-active');
+        button[0].classList.remove('button-disabled');
+        button[1].classList.add('button-active');
+        button[1].classList.remove('button-disabled');
+    }
+    if(window.scrollY == 0){
+        button[0].classList.add('button-disabled');
+        button[0].classList.remove('button-active');
+        button[1].classList.add('button-disabled');
+        button[1].classList.remove('button-active');
+    }
+})
 slideShow();
 
